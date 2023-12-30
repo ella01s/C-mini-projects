745b40636e424081ef1b06d72f69c79bc92ad7cc
ella01s <85433513+ella01s@users.noreply.github.com>
2023-12-30 21:15:44 +0000

    Add files via upload
    This project takes the users age and asks if they want to convert their age into dog or cat years. the program does the conversion based on the users response

---
+++ ageConvertionHumanToAnimal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Review
+{
+  class Program 
+  {
+    static double humanAge;
+    static double catAge;
+
+    static void Main(string[] args)
+    {
+      getAge();
+    }
+
+    public static void getAge(){
+      Console.WriteLine("How old are you?");
+      string userAge = Console.ReadLine();
+      humanAge = Convert.ToDouble(userAge);
+
+      Console.WriteLine("Would you like to convert your age into cat or dog years?");
+      string response = Console.ReadLine();
+
+      if(response.ToLower()=="cat"){
+        convertAgeCat();
+      }else{
+        convertAgeDog();
+      }
+    }
+
+    public static void convertAgeDog(){
+      double dogAge = Math.Exp((humanAge-31.0)/16.0);
+      Console.WriteLine($"you are {dogAge} years old in dog years");
+        
+    }
+
+    public static void convertAgeCat(){
+        if(humanAge <= 15){
+            catAge = 1;
+        }else if( humanAge>15 & humanAge<= 24){
+            catAge = 2;
+        }else{
+            double catAge = ((humanAge - 24) / 4) + 2;
+        }Console.WriteLine($"your are {catAge} years old in cat years");
+    }
+ }
+  
+}
