d84d677b02069cd2287bc4740166be8809acd055
ella01s <85433513+ella01s@users.noreply.github.com>
2024-01-18 21:14:55 +0000

    Add files via upload
    The program takes a block of text from the user and converts it to an array. It then asks the user to pick a letter and generates a poem using that letter by iterating over the provided block of text to find all the words that begin with the users chosen letter.

---
+++ poemGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Metadata;
+
+namespace poemGen{
+    class Program{
+        static string userText;
+        static string letter;
+        static void Main (string[]args){
+            getText();
+            findWord();
+
+        }
+
+        static void getText(){
+            Console.WriteLine("Let's write a poem. Please copy a block of text into the terminal. provide a minimum of one paragraph. we will use the words in this text to make poem");
+            userText=Console.ReadLine();
+            Console.WriteLine("choose one letter of the alphabet and type it to the console");
+            letter=Console.ReadLine();
+        }
+
+        static void wordFound(string word){
+            Console.WriteLine(word);
+        }
+        
+        static void findWord(){
+            string[] words = userText.Split(' ', StringSplitOptions.RemoveEmptyEntries); //.Split to split the string into an array
+            Console.WriteLine("Your peom:");
+
+            foreach(string word in words){
+                if (word.StartsWith(letter, StringComparison.OrdinalIgnoreCase)){
+                    wordFound(word);
+                }
+
+            }Console.WriteLine("THE END");
+
+        }
+
+
+
+    }
+}
